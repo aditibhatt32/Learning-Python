9dc587acf1db2dccde85207a94b2cae1c5b460f7
aditibhatt32 <67057405+aditibhatt32@users.noreply.github.com>
2021-07-25 22:53:19 +0530

    Program using dictionary

---
+++ dict.py
@@ -0,0 +1,8 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> dict={'name':'Aditi','branch':'cse','learning':'python'}
+>>> print('about me :',dict)
+about me : {'name': 'Aditi', 'branch': 'cse', 'learning': 'python'}
+>>> print('what is your branch:',dict[branch])
+del dict['learning']
+print(dict)
